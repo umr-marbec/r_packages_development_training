09907a158b35c1d6df4f144d646163fd75052321
MathieuDepetris <mathieu.depetris@cnrs.fr>
2025-09-03 08:04:32 +0200

    Update common

--- common.R
+++ common.R
@@ -1,3 +1,9 @@
 library(dplyr)
-usethis::create_package(path = path.expand("~/macfly"))
+path_package <- path.expand("~/macfly")
+if (dir.exists(paths = path_package)) {
+  unlink(path_package,
+         recursive = TRUE)
+}
+usethis::create_package(path = path_package,
+                        open = FALSE)
 knitr::opts_knit$set(root.dir = "~/macfly")
